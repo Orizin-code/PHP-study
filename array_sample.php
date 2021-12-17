73cb58292650a72c5ab4d4b680f6514583af4305
Orizin <nlgporizin10@gmail.com>
2021-12-17 14:22:52 +0900

    配列

---
+++ array_sample.php
@@ -0,0 +1,31 @@
+<?php //配列は0から数える
+     //要素[   0   ,    1    ,    2    ,    3    ]
+$users = ["Saitou", "Tanaka", "Suzuki", "Yamada"];
+
+echo $users[2];
+
+?>
+
+<?php //連想配列
+
+$fruits = [
+    "banana" => "バナナ",
+    "apple" => "りんご",
+    "orange" => "みかん",
+    "grape" => "ぶどう"
+];
+
+echo $fruits["apple"];
+
+?>
+
+<?php //数字も格納可能
+
+$ages = [
+    "父" => 55,
+    "母" => 52,
+    "兄" => 33,
+    "妹" => 28  
+];
+
+?>
