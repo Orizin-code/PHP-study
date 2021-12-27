458781fdbdefe8c43affe05f7046574853373e6f
Orizin <nlgporizin10@gmail.com>
2021-12-27 10:53:31 +0900

    クラスを用いた商品表示

--- class_program_sample.php
+++ class_program_sample.php
@@ -1,32 +1,71 @@
 <?php
 
-$array = ['name' => '田中', 'age' => 17];
-
-class Student
+class Product
 {
+    public $id;
     public $name;
-    public $age;
+    public $price;
+    // クラス変数を定義
+    public static $count = 0;
 
     public function __construct(array $array)
     {
+        $this->id = Product::$count += 1;
         $this->name = $array['name'];
-        $this->age = $array['age'];
+        $this->price = $array['price'];
+    }
+}
+
+// Greengrocerクラスを定義
+class Greengrocer {
+
+    // インスタンス生成と同時に$productsを定義
+    public $products;
+
+    public function __construct($productParams)
+    {
+        $this->registerProduct($productParams);
     }
 
-    // インスタンスメソッドの定義
-    public function greet(){
-        echo "はじめまして！";
+    // 商品を登録
+    public function registerProduct($productParams)
+    {
+        foreach ($productParams as $productParam) {
+            $this->products[] = new Product($productParam);
+        }
     }
 
-    // インスタンスメソッドの定義
-    public function introduce(){
-        echo "私は{$this->name}です。年齢は{$this->age}歳です。";
+    // 商品を表示
+    public function dispProducts()
+    {
+        echo "いらっしゃいませ！商品を選んでください。";
+        echo PHP_EOL;
+
+        foreach ($this->products as $product) {
+            echo $product->id . "." . $product->name . "（" . $product->price . "円）";
+            echo PHP_EOL;
+        }
     }
 
 }
 
-// インスタンスメソッドの呼び出し
-$student = new Student($array);
-$student->greet();
-echo PHP_EOL;
-$student->introduce();
+// 商品データ1
+$productParams1 = [
+    ['name' => 'トマト', 'price' => '100'],
+    ['name' => 'きゅうり', 'price' => '200'],
+    ['name' => '玉ねぎ', 'price' => '300'],
+    ['name' => 'なす', 'price' => '400']
+];
+
+// 追加商品データaddingProductParams1を定義
+$addingProductParams1 = [
+    ['name' => 'ごぼう', 'price' => '250'],
+    ['name' => 'れんこん', 'price' => '350']
+];
+
+// Greengrocerクラスインスタンス生成
+$greengrocer1 = new Greengrocer($productParams1);
+// 商品を登録するメソッドを呼び出す
+$greengrocer1->registerProduct($addingProductParams1);
+// 商品を表示
+$greengrocer1->dispProducts();
